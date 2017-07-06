88e0ea1702400e17aa28f16cf4a27a6079d02a72
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2017-07-05 21:39:03 -0300

    Adição dos logos e ajuste dos menus

--- src/main/java/br/com/rar/superafit/superafitbackoffice/controller/PrincipalController.java
+++ src/main/java/br/com/rar/superafit/superafitbackoffice/controller/PrincipalController.java
@@ -1,38 +1,22 @@
 package br.com.rar.superafit.superafitbackoffice.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import br.com.rar.superafit.superafitbackoffice.controller.model.Pessoa;
 
 @Controller
-@RequestMapping("/principal")
 public class PrincipalController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public ModelAndView novo(Pessoa pessoa) {
-		ModelAndView mav = new ModelAndView("principal");
-		return mav;
+	@RequestMapping("/")
+	public ModelAndView root() {
+		return new ModelAndView("redirect:/principal");
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult result
-			, RedirectAttributes attributes) {
-		
-		if (result.hasErrors()) {
-			return novo(pessoa);
-		}
-		
-		ModelAndView mv = new ModelAndView("redirect:/principal");
-		attributes.addFlashAttribute("mensagem", "Pessoa salva com sucesso.");
-		return mv;
-		
-	}	
+	@RequestMapping(value="principal", method=RequestMethod.GET)
+	public ModelAndView novo() {
+		ModelAndView mav = new ModelAndView("principal/principal");
+		return mav;
+	}
 	
 }
